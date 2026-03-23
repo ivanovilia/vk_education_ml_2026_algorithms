4e11e2f97de7e2b58b2abf4c037b4fdcae88862c
ivanovilia <63732630+ivanovilia@users.noreply.github.com>
2026-03-23 20:33:57 +0300

    Add files via upload

---
+++ first_task.py
@@ -0,0 +1,40 @@
+
+class ListNode:
+    def __init__(self,value):
+        self.value = value
+        self.next = None
+
+def intersection(listA, listB):
+    pointerA, pointerB = listA, listB
+
+    while pointerA != pointerB:
+        
+        if pointerA:
+            pointerA = pointerA.next
+        else:
+            pointerA = listB
+
+        if pointerB:
+            pointerB = pointerB.next
+        else:
+            pointerB = listA    
+            
+    return pointerA    
+
+
+commonPart = ListNode(1)
+commonPart.next = ListNode(8)
+commonPart.next.next = ListNode(4)
+commonPart.next.next.next = ListNode(5)
+
+A = ListNode(4)
+A.next = commonPart
+
+B = ListNode(5)
+B.next = ListNode(6)
+B.next.next = commonPart
+
+result = intersection(A, B)
+if result:
+    print(result, result.value)
+
